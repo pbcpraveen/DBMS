1468e2866f0f7a7f05cfd0380aa6c7127b7398ac
Praveen Kumar R <42463334+pbcpraveen@users.noreply.github.com>
2019-02-09 11:26:20 +0530

    updated

--- Ex04/Ex04_commands.sql
+++ Ex04/Ex04_commands.sql
@@ -10,18 +10,18 @@ select * from Blue_flavor;
 select COLUMN_NAME, UPDATABLE from USER_UPDATABLE_COLUMNS where table_name = 'BLUE_FLAVOR';
 
 REM : Checking whether it is upadteble or not
-insert into products values('74-ABC-7U','Blueberry','Icecream',2.34);
+insert into products values('74-A99C-7U','Blueberry','Icecream',2.34);
 select * from Blue_flavor;
 
 update Blue_flavor
 set price = price+2
-where food = 'Tart'
+where food = 'Tart';
 
 REM : upadte successful
 select * from Blue_flavor;
 
 delete from Blue_flavor 
-where food = 'Tart';
+where pid = '74-A99C-7U';
 
 
 
@@ -54,8 +54,6 @@ where food = 'icecream';
 
 select * from cheap_food;
 
-delete from cheap_food 
-where food = 'Cookie';
 
 delete from cheap_food 
 where food = 'icecream';
@@ -66,7 +64,7 @@ select COLUMN_NAME, UPDATABLE from USER_UPDATABLE_COLUMNS where table_name = 'CH
 
 
 REM 3. Create a view called Hot_Food that show the product id and its quantity where the
-       same product is ordered more than once in the same receipt.
+REM    same product is ordered more than once in the same receipt.
 create view hot_food as (
 select i.item, count(*) as quantity 
 from item_list i join receipts r on i.rno = r.rno 
@@ -78,9 +76,15 @@ where i.item = any(select i2.item
 group by i.item);
 select COLUMN_NAME, UPDATABLE from USER_UPDATABLE_COLUMNS where table_name = 'HOT_FOOD';
 
+insert into hot_food values ('A-B-c',3);
+
+delete from hot_food
+where item = 'A-B-c';
+
 update hot_food 
 set quantity = quantity+2 
 where item = '46-11';
+
 
 REM 4. Create a view named Pie_Food that will display the details (customer lname, flavor,
 REM    receipt number and date, ordinal) who had ordered the Pie food with receipt details.
@@ -92,6 +96,13 @@ where p.food = 'Pie');
 select * from pie_food;
 select COLUMN_NAME, UPDATABLE from USER_UPDATABLE_COLUMNS where table_name = 'PIE_FOOD';
 
+insert into pie_food values('praven','berry',6483,'20-Jan-2005',7);
+delete from pie_food 
+where flavor = 'berry';
+update pie_food 
+set ordinal = ordinal+1 
+where flavor = 'berry';
+
 REM 5. Create a view Cheap_View from Cheap_Food that shows only the product id, flavor
 REM    and food.
 
@@ -100,6 +111,20 @@ select pid , flavor, food
 from cheap_food );
 
 select * from cheap_view;
+select COLUMN_NAME, UPDATABLE from USER_UPDATABLE_COLUMNS where table_name = 'CHEAP_VIEW';
+
+insert into cheap_view values('Ac-RD-HG','gum','fondant');
+
+update cheap_view
+set flavor = 'grapes'
+where flavor = 'gum';
+
+select * form cheap_view;
+
+select * from cheap_food;
+
+delete from cheap_food
+where pid= 'Ac-RD-HG';
 
 
 REM 6. Create a sequence named Ordinal_No_Seq which generates the ordinal number
@@ -138,6 +163,20 @@ for item_list;
 select *
 from product_details
 where item =  '90-APR-PF';
+
+insert into product_details values(insert into item_list values(91937, 8,  '51-BC');
+
+update product_details
+set ordinal = ordinal+1
+where rno=91937 and ordinal = 8;
+
+select * from item_list
+where rno = 91937;
+
+delete from product_details
+where rno = 91937 and ordinal = 8;
+
+
 
 REM 8. Drop all the above created database objects.
 
