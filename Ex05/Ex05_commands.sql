b7002679b2ca5eb4877327a163701bc5f7a9f36e
Praveen Kumar R <42463334+pbcpraveen@users.noreply.github.com>
2019-02-26 15:30:42 +0530

    Update Ex05_commands.sql

--- Ex05/Ex05_commands.sql
+++ Ex05/Ex05_commands.sql
@@ -1,44 +1,52 @@
 REM 1. Check whether the given combination of food and flavor is available. If any one or
 REM    both are not available, display the relevant message.
 set serveroutput on;
-create or replace procedure select_1(fl IN products.flavor%type, fo IN products.food%type, count_1 OUT integer) IS 
-begin
-	select count(*) into count_1
-	from (select pid from products 
-	where food=fo and flavor=fl);
-end;
-/
+
 declare
 fl products.flavor%type;
 fo products.food%type;
-c number(3);
+c1 number(3);
+c2 number(3);
+c3 number(3);
+
 begin	
 	fl := &flavor;
 	fo := &food;
-	select_1(fl,fo,c);
-	if SQL%found and c>0 then
-		dbms_output.put_line(c||' found');
-	else
-		dbms_output.put_line('Not found');
+	select count(*) into c1
+	from (select * from products p where p.food = fo and p.flavor = fl);
+    select count(*) into c2
+	from (select * from products p where p.flavor = fl);
+    select count(*) into c3
+	from (select * from products p where p.food = fo);
+	if c1>0 then
+		dbms_output.put_line(c1||' products found of given combination');
+		return;
 	end if;
+    if c2>0 then
+		dbms_output.put_line('only flavor is found');
+		return;
+		end if;
+	if c3>0 then
+		dbms_output.put_line('only food is found');
+		return;
+		end if;
+	dbms_output.put_line('neither food nor flavor is found');
+
 end;
 /
 REM 2. On a given date, find the number of items sold (Use Implicit cursor).
-create or replace procedure select_2(date_1 IN receipts.r_date%type, count_1 OUT integer) IS 
-begin
-	select count(*) into count_1
-	from (select r_date from item_list i join receipts r on r.rno=i.rno 
-	where r.r_date=date_1);
-end;
-/
+
 declare
 date_search receipts.r_date%type;
-c number(3);
-begin	
-	date_search := &date_search;
-	select_2(date_search,c);
-	if SQL%found and c>0 then
-		dbms_output.put_line('No. of items sold on '||date_search||' is/are:'||c);
+begin
+    
+	date_search := '&date_search';
+	update item_list i
+	set i.ordinal = i.ordinal+0
+	where i.rno in (select r.rno from receipts r
+	                            where r.r_date = date_search);
+	if SQL%found and SQL%rowcount >0 then
+		dbms_output.put_line('No. of items sold on '||date_search||' is/are:'||SQL%rowcount);
 	else
 		dbms_output.put_line('No items sold');
 	end if;
